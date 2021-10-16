ea1479912979756586540f0374fe3465df38f831
Vlass Contreras <17748144+vlasscontreras@users.noreply.github.com>
2021-10-16 09:31:19 -0600

    🤡 Add unstable versions of SRP and OCP

---
+++ src/SingleResponsibility/Unstable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Vlass\Solid\SingleResponsibility;
+
+use DateTime;
+
+class Unstable
+{
+    /**
+     * Log message to file
+     *
+     * @param string        $level
+     * @param string        $message
+     * @param DateTime|null $dateTime
+     * @return void
+     */
+    public function log(string $level, string $message, ?DateTime $dateTime = null): void
+    {
+        $dateTime = $dateTime ?? new DateTime();
+
+        $logMessage = sprintf(
+            '[%1$s] %2$s: %3$s',
+            $dateTime->format('Y-m-d H:i:s'),
+            $level,
+            $message
+        );
+
+        file_put_contents('log.txt', $logMessage, FILE_APPEND);
+    }
+}
